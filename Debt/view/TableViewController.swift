dae94d0f35315b0e6cdb72d80210918c38e103e2
Mairambek Abdrasulov <mairambek.abdrasulov@gmail.com>
2019-05-31 17:58:51 +0600

    app bar button item

--- Debt/view/TableViewController.swift
+++ Debt/view/TableViewController.swift
@@ -8,16 +8,40 @@
 
 import UIKit
 
-class TableViewController: UITableViewController {
+class TableViewController: UITableViewController,UITextFieldDelegate  {
 
     @IBOutlet weak var name: UITextField!
     @IBOutlet weak var lastName: UITextField!
     @IBOutlet weak var sum: UITextField!
-    @IBOutlet weak var dateOut: UIDatePicker!
+    @IBOutlet weak var dataOut: UIDatePicker!
     @IBOutlet weak var dateIn: UIDatePicker!
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.name.delegate = self
+        self.lastName.delegate = self
+        self.sum.delegate = self
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done , target: self, action: #selector(selectorBarButtonItem (paramSender:)))
     }
+
+    //MARK: - Method
+    
+    //нужно дать работу кнопке, что бы данные сохранялись
+    
+    @objc func selectorBarButtonItem (paramSender: Any){
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    //MARK: - textFieldDelegate
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.name.resignFirstResponder()
+        self.lastName.resignFirstResponder()
+        self.sum.resignFirstResponder()
+        return true
+        
+    }
+    
 
 }
--- Debt/view/ViewController.swift
+++ Debt/view/ViewController.swift
@@ -38,5 +38,6 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
 
+
 }
 
